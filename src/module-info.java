c874b2cf767c9cb883e6728b409d6a2a292013f1
Baptiste <baptisteperrin77@gmail.com>
2019-12-04 15:36:51 +0100

    Initial Commit

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+module application{
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.media;
+	requires javafx.swing;
+	requires javafx.web;
+	
+	exports application;
+	opens application to javafx.graphics;
+}
