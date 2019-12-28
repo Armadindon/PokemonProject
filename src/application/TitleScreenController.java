5b1d0a36805276faca044fe16d9d53f9919af0f4
Kwaaac <max.rodriguezodf@gmail.com>
2019-12-28 13:32:50 +0100

    TitleScreen
    Il manque:
    
    -l'img de Baptiste
    -KeyEvent chelou à moitié bugué

--- src/application/TitleScreenController.java
+++ src/application/TitleScreenController.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -12,7 +13,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -27,19 +27,19 @@ public class TitleScreenController {
 	private URL location;
 
 	@FXML
-	private ImageView imgArmadindon;// return at x: 730
+	private ImageView imgArmadindon;
 
 	@FXML
-	private ImageView imgKwaaac; // return at x: 169
+	private ImageView imgKwaaac;
 
 	@FXML
 	private ImageView imgPokemon;
 
 	@FXML
-	private Label labelArmadindon; // return at x: 616
+	private Label labelArmadindon;
 
 	@FXML
-	private Label labelKwaac; // return at x: 67
+	private Label labelKwaaac;
 
 	@FXML
 	private Label labelPressAnyKey;
@@ -49,28 +49,67 @@ public class TitleScreenController {
 
 	@FXML
 	private AnchorPane root;
+	
+	
+	@FXML
+	private AnchorPane anchorPFade;
 
 	@FXML
 	void changeToGame(MouseEvent event) throws IOException {
-	
+
 		Parent root = FXMLLoader.load(getClass().getResource("interface.fxml"));
 		Scene moveScene = new Scene(root);
 
-		
 		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
 		window.setScene(moveScene);
 		window.show();
 	}
 
+	private void pokeFade() {
+		FadeTransition ft1 = new FadeTransition(new Duration(1_000), anchorPFade);
+		ft1.setFromValue(0.0);
+		ft1.setToValue(1.0);
+		ft1.setAutoReverse(true);
+		ft1.play();
+		
+		FadeTransition ft2 = new FadeTransition(new Duration(1_000), labelPressAnyKey );
+		ft2.setFromValue(0.0);
+		ft2.setToValue(1.0);
+		ft2.setCycleCount(10_000);
+		ft2.setAutoReverse(true);
+		ft2.play();
+		
+		
+		TranslateTransition tImgKwaaac = new TranslateTransition(new Duration(2_000), imgKwaaac);
+		tImgKwaaac.setByX(169);
+		tImgKwaaac.play();
+		
+		TranslateTransition tLabelKwaaac = new TranslateTransition(new Duration(2_000), labelKwaaac);
+		tLabelKwaaac.setByX(100);
+		tLabelKwaaac.play();
+		
+		TranslateTransition tImgArmadindon = new TranslateTransition(new Duration(2_000), imgArmadindon);
+		tImgArmadindon.setByX(-169);
+		tImgArmadindon.play();
+		
+		TranslateTransition tLabelArmadindon= new TranslateTransition(new Duration(2_000), labelArmadindon);
+		tLabelArmadindon.setByX(-100);
+		tLabelArmadindon.play();
+		
+	}
+
 	@FXML
 	void initialize() {
-		FadeTransition ft = new FadeTransition(new Duration(10_000), root);
+
+		// Duration time while the sreen display void
+
+		FadeTransition ft = new FadeTransition(new Duration(3_000), anchorPFade);
 		ft.setFromValue(0.0);
-		ft.setToValue(1.0);
+		ft.setToValue(0.0);
+		ft.setOnFinished(e -> pokeFade());
 
 		ft.play();
-
 	}
 
 }
