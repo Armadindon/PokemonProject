799182e48bc31b97fb0070b7679f0b979ea2b786
Kwaaac <max.rodriguezodf@gmail.com>
2019-12-29 23:05:19 +0100

    fxml Fight - progressBar + fx:id

--- src/application/FightController.java
+++ src/application/FightController.java
@@ -2,60 +2,121 @@ package application;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Tab;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 
 public class FightController {
 
-	@FXML
-	private ResourceBundle resources;
+    @FXML
+    private ResourceBundle resources;
 
-	@FXML
-	private URL location;
+    @FXML
+    private URL location;
 
-	@FXML
-	private HBox hboxSwitchPokemon0;
+    @FXML
+    private HBox hBoxMenuButtons;
 
-	@FXML
-	private HBox hboxSwitchPokemon1;
+    @FXML
+    private HBox hboxSwitchPokemon0;
 
-	@FXML
-	private HBox hboxSwitchPokemon2;
+    @FXML
+    private HBox hboxSwitchPokemon1;
 
-	@FXML
-	private HBox hboxSwitchPokemon3;
+    @FXML
+    private HBox hboxSwitchPokemon2;
 
-	@FXML
-	private HBox hboxSwitchPokemon4;
+    @FXML
+    private HBox hboxSwitchPokemon3;
 
-	@FXML
-	private HBox hboxSwitchPokemon5;
+    @FXML
+    private HBox hboxSwitchPokemon4;
 
-	@FXML
-	private AnchorPane root;
+    @FXML
+    private HBox hboxSwitchPokemon5;
 
-	@FXML
-	private TabPane tabPaneMenu;
+    @FXML
+    private Label labelAllyPokeHP;
 
-	@FXML
-	private Tab tabBP;
+    @FXML
+    private Label labelAllyPokeLvl;
 
-	@FXML
-	private Tab tabMenu;
+    @FXML
+    private Label labelAllyPokeName;
 
-	@FXML
-	private Tab tabMove;
+    @FXML
+    private Label labelFoePokeHP;
 
-	@FXML
-	private Tab tabSwitch;
+    @FXML
+    private Label labelFoePokeLvl;
 
-	@FXML
-	private Tab tabText;
+    @FXML
+    private Label labelFoePokeName;
 
+    @FXML
+    private ProgressBar progressBarAllyPokeHP;
+
+    @FXML
+    private ProgressBar progressBarFoePokeHP;
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private TabPane tabPaneMenu;
+
+    @FXML
+    private TextArea textAreaMatchNotification;
+
+    @FXML
+    private VBox vBoxMove0;
+
+    @FXML
+    private VBox vBoxMove1;
+
+    @FXML
+    private VBox vBoxMove2;
+
+    @FXML
+    private VBox vBoxMove3;
+
+
+    @FXML
+    void backPackPage(MouseEvent event) {
+    }
+
+    @FXML
+    void fightMenu(MouseEvent event) {
+    }
+
+    @FXML
+    void run(MouseEvent event) {
+    }
+
+    @FXML
+    void switchMenu(MouseEvent event) {
+    }
+
+    @FXML
+    void switchPokemon(MouseEvent event) {
+    }
+
+    @FXML
+    void useMove(MouseEvent event) {
+    }
+
+    @FXML
+    void initialize() {
+
+
+
+    }
 
 }
